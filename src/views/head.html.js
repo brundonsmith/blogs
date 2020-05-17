c4a7b76b8903e03a63d4d456dd54ddf4e548267d
Brandon Smith <brundolf@Brandons-MBP.lan1>
2020-05-16 20:57:07 -0500

    Made css/js paths non-absolute

--- src/views/head.html.js
+++ src/views/head.html.js
@@ -11,7 +11,7 @@ module.exports = () =>
             <meta charset="UTF-8">
             <meta name="viewport" content="width=device-width, initial-scale=1.0" />
 
-            <link rel="stylesheet" type="text/css" href="/site.css">
-            <script src="/site.js"></script>
+            <link rel="stylesheet" type="text/css" href="site.css">
+            <script src="site.js"></script>
         </head>
     `
